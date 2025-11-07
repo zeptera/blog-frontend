a983c2fcb91258cb4ea84c8c1a1c17049ae30c45
HoodaHashem <mahmoud.hashem.dev@gmail.com>
2025-11-07 22:29:10 +0200

    Feat: react router added

---
+++ src/router/index.ts
@@ -0,0 +1,40 @@
+import { createBrowserRouter, redirect } from "react-router";
+import RootLayout from "../pages/rootLayout";
+import AuthLayout from "../pages/auth/layout.tsx";
+import Home from "../pages/home";
+import Login from "../pages/auth/login.tsx";
+import Signup from "../pages/auth/signup.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RootLayout,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+    ],
+  },
+  {
+    path: "auth",
+    Component: AuthLayout,
+
+    children: [
+      {
+        index: true,
+        loader: () => redirect("/auth/login"),
+      },
+      {
+        path: "login",
+        Component: Login,
+      },
+      {
+        path: "signup",
+        Component: Signup,
+      },
+    ],
+  },
+]);
+
+export default router;
