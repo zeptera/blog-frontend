fa796c7a907494323a437271af1a5e676e5e30d8
HoodaHashem <mahmoud.hashem.dev@gmail.com>
2025-11-07 22:30:02 +0200

    Feat: app config and migration with tailwind and shadcn

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { RouterProvider } from "react-router";
+import router from "./router";
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;
