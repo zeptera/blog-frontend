05439ef6639ccc95c0c6ae57d4b1b087baf399f2
HoodaHashem <mahmoud.hashem.dev@gmail.com>
2025-11-08 23:46:36 +0200

    Feat: adding new font and improving UI/UX for auth layout

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ import router from "./router";
 function App() {
   return (
     <>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Funnel+Display:wght@400;700&display=swap"
+        rel="stylesheet"
+      />
       <RouterProvider router={router} />
     </>
   );
--- src/pages/auth/layout.tsx
+++ src/pages/auth/layout.tsx
@@ -1,9 +1,18 @@
-import { Outlet } from "react-router";
+import { Link, Outlet } from "react-router";
+import zeptera from "../../assets/zeptera.svg";
 
 const AuthLayout = () => {
   return (
     <div className="w-full h-screen flex  justify-center items-center">
-      <Outlet />
+      <div className="flex flex-col gap-10">
+        <Link to={"/"} className="flex justify-cneter items-center gap-4">
+          <img src={zeptera} alt="logo" className="w-16" />
+          <h1 className="font-funnel text-4xl font-bold text-primary">
+            Zeptera
+          </h1>
+        </Link>
+        <Outlet />
+      </div>
     </div>
   );
 };
